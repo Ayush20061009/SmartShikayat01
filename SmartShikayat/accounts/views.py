0409241d66ad1680aeb9962c4ce3efde69912cf2
Kadiya Ayush <ayushhkadiya@gmail.com>
2026-02-13 08:57:10 +0530

    UI Done

--- SmartShikayat/accounts/views.py
+++ SmartShikayat/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.db import IntegrityError  # Import IntegrityError
 from .forms import CitizenSignUpForm, OfficerSignUpForm
 from django.views.generic import TemplateView
 from notifications.utils import send_welcome_email
@@ -11,10 +12,17 @@ def citizen_signup(request):
     if request.method == 'POST':
         form = CitizenSignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            send_welcome_email(user)
-            return redirect('complaint_list')
+            try:
+                user = form.save()
+                login(request, user)
+                # Wrap email sending to prevent crashing if email fails
+                try:
+                    send_welcome_email(user)
+                except Exception:
+                    pass 
+                return redirect('complaint_list')
+            except IntegrityError:
+                form.add_error('username', 'This username is already taken. Please choose another.')
     else:
         form = CitizenSignUpForm()
     return render(request, 'registration/citizen_signup.html', {'form': form})
@@ -23,10 +31,17 @@ def officer_signup(request):
     if request.method == 'POST':
         form = OfficerSignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            send_welcome_email(user)
-            return redirect('officer_dashboard')
+            try:
+                user = form.save()
+                login(request, user)
+                try:
+                    send_welcome_email(user)
+                except Exception:
+                    pass
+                return redirect('officer_dashboard')
+            except IntegrityError:
+                # This catches the specific error you are seeing
+                form.add_error('username', 'This username is already taken. Please choose another.')
     else:
         form = OfficerSignUpForm()
     return render(request, 'registration/officer_signup.html', {'form': form})
